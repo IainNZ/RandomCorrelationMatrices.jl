1846c6abb9e54a79064429fd3ec812da0de7cb7c
Iain Dunning <iaindunning@gmail.com>
2015-07-07 01:23:37 -0400

    Implement LKJ method

--- src/RandomCorrelationMatrices.jl
+++ src/RandomCorrelationMatrices.jl
@@ -1,5 +1,37 @@
 module RandomCorrelationMatrices
 
-# package code goes here
+    using Distributions
+    
+    # Based on
+    # Lewandowski, Kurowicka, Joe
+    # "Generating random correlation matrices based on vines and extended onion method"
+    # Journal of Multivariate Analysis 100 (2009)
+    # doi:10.1016/j.jmva.2009.04.008
+    export randcormatrix
+    function randcormatrix(d, η)
+        β = η + (d-2)/2
+        u = rand(Beta(β,β))
+        r₁₂ = 2*u - 1
+        r = [   1 r₁₂ ;
+              r₁₂   1 ]
+        # In published paper this index is n = 2:d-1
+        # n is never mentioned again, but a mysterious k is
+        # We'll thus use k
+        # Similar approach was taken here:
+        # http://stats.stackexchange.com/a/125017/58921
+        for k in 2:d-1
+            β = β - 1/2
+            y = rand(Beta(k/2,β))
+            u = randn(k)
+            u /= norm(u)
+            w = sqrt(y)*u
+            A = chol(r, :L)
+            z = A*w
+            r = [ r  z ;
+                  z' 1 ]
+        end
+        return r
+    end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,24 @@
 using RandomCorrelationMatrices
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+srand(12345)
+n = 4
+for η in [2,8,32,128]
+    r = randcormatrix(n,η)
+    @show η
+    for i in 1:n
+        for j in 1:n
+            @printf("%5.2f ", r[i,j])
+        end
+        println()
+    end
+
+    ranges = Float64[]
+    for s in 1:1000
+        r = randcormatrix(n,η)
+        r0 = r - diagm(diag(r))
+        push!(ranges, maximum(vec(r0)) - minimum(vec(r0)))
+    end
+    @show mean(ranges)
+    @show median(ranges)
+end
