c3e7582c17d47b4224944f9f58f55a681a767c18
Iain Dunning <iaindunning@gmail.com>
2018-07-06 17:39:31 -0400

    Make a limited test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@ import LinearAlgebra: cholesky, diag, Diagonal, Hermitian
 import Printf: @printf
 import Random
 import Statistics: mean, median, std
+using Test
 import RandomCorrelationMatrices
+
 # This is not a numerical test. The code just generates some examples.
 # However, if it fails to run, thats a good sign something is wrong.
-
 Random.srand(12345)
-
 println("Random correlation matrix")
 n = 4
 for η in [2, 8, 32, 128]
@@ -31,15 +31,20 @@ for η in [2, 8, 32, 128]
 end
 
 
-println("\nRandom covariance matrix")
-σ = [100, 200, 300, 400, 500]
-@show σ
-Σ = Hermitian(RandomCorrelationMatrices.randcovmatrix(5, 10, σ))
-L = cholesky(Σ).L
-samples = 100_000
-results = zeros(5, samples)
-for i in 1:samples
-    results[:,i] = L*randn(5)
+@testset "Random covariance matrix" begin
+    Random.srand(12345)
+    σ = [100, 200, 300, 400, 500]
+    Σ = RandomCorrelationMatrices.randcovmatrix(5, 10, σ)
+    L = cholesky(Hermitian(Σ)).L
+    samples = 100_000
+    results = zeros(5, samples)
+    for i in 1:samples
+        results[:,i] = L*randn(5)
+    end
+    means = mean(results, dims=2)
+    @test all(abs.(means) .<= 5)
+    stddevs = std(results, dims=2)
+    @test all(abs.(stddevs - σ) .<= 5)
+    @show σ
+    @show round.(stddevs, digits=1)
 end
-@show round.(mean(results, dims=2), digits=1)
-@show round.(std(results, dims=2), digits=1)
